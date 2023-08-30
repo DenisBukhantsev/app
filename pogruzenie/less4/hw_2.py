c80b3c07f00a204dca282a3fc9d1af47868e8f2d
DenisBukhantsev <118934730+DenisBukhantsev@users.noreply.github.com>
2023-08-30 15:01:12 +0300

    Update hw_2.py

--- pogruzenie/less4/hw_2.py
+++ pogruzenie/less4/hw_2.py
@@ -3,6 +3,11 @@
 def my_func2(**args):
     val = args.keys()
     key = args.values()
-    result = {str(x): y for x, y in zip(key, val)}
+    result = {} #{str(x): y for x, y in zip(key, val)} это выражение в одну строку но тут все ключи будут str
+    for x, y in zip(key, val):
+        if type(x) != int:
+            result[str(x)] = y 
+        else:
+            result[x] = y
     return result
 print(my_func2(a = 2, w = "sdfs", goal = ["dsdf", "ваыва", 111]))
